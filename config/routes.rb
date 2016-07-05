957c4e7b2c2f7adc8620285d38048b8995545c7b
DeShawn Cousette <dcousette@gmail.com>
2016-07-05 12:29:08 -0400

    add routes for new app

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/', to: 'static_pages#home'
-  
+
   get     'signin',  to: 'sessions#new'
   post    'signin',  to: 'sessions#create'
   delete  'signout', to: 'sessions#destroy'
   get     'home',    to: 'static_pages#home'
   get     'search',  to: 'jobs#search'
-  
-  resources :users 
-  
-  resources :companies, only: [:new, :create] do 
-    resources :jobs, only: [:new, :create] 
+
+  resources :users
+
+  resources :companies, only: [:new, :create] do
+    resources :jobs, only: [:new, :create]
   end
-  
-  resources :jobs, only: [:show]
+
+  resources :jobs, only: [:show] do
+    resources :applications, only: [:new, :create]
+  end
 end
