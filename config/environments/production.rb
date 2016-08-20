1dcd61bfcbcb6009f770b364148039dfdd1ef549
DeShawn Cousette <dcousette@gmail.com>
2016-08-19 23:45:15 -0400

    fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     port:             ENV['MAILGUN_SMPT_PORT'],
     address:          ENV['MAILGUN_SMPT_SERVER'],
-    user_name:        ENV['MAILGUN_SMTP_LOGIN']
+    user_name:        ENV['MAILGUN_SMTP_LOGIN'],
     password:         ENV['MAILGUN_SMPT_PASSWORD'],
     domain:          'jobberapp.heroku.com',
     authentication:  :plain
