4cff6912c7284bb0a41126626ce7e0572572bf46
DeShawn Cousette <dcousette@gmail.com>
2016-08-20 00:07:52 -0400

    fix typos in config/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,10 +74,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    port:             ENV['MAILGUN_SMPT_PORT'],
-    address:          ENV['MAILGUN_SMPT_SERVER'],
+    port:             ENV['MAILGUN_SMTP_PORT'],
+    address:          ENV['MAILGUN_SMTP_SERVER'],
     user_name:        ENV['MAILGUN_SMTP_LOGIN'],
-    password:         ENV['MAILGUN_SMPT_PASSWORD'],
+    password:         ENV['MAILGUN_SMTP_PASSWORD'],
     domain:          'jobberapp.heroku.com',
     authentication:  :plain
   }
